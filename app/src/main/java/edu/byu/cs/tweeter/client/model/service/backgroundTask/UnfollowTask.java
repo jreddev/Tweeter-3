29908f26407a2a268af101e1b0aaf903fa3ae0d8
Jared <jared.devenport@gmail.com>
2023-03-21 10:15:20 -0600

    finised unfollow task api

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/UnfollowTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/UnfollowTask.java
@@ -2,8 +2,13 @@ package edu.byu.cs.tweeter.client.model.service.backgroundTask;
 
 import android.os.Handler;
 
+import java.io.IOException;
+
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.net.request.FollowRequest;
+import edu.byu.cs.tweeter.model.net.response.FollowResponse;
 
 /**
  * Background task that removes a following relationship between two users.
@@ -19,5 +24,25 @@ public class UnfollowTask extends AuthenticatedTask {
     public UnfollowTask(AuthToken authToken, User followee, Handler messageHandler) {
         super(authToken,messageHandler);
         this.followee = followee;
+    }
+
+    @Override
+    public void runTask() throws IOException {
+        try {
+            String followeeAlias = followee == null ? null : followee.getAlias();
+
+            FollowRequest request = new FollowRequest(authToken, followeeAlias);
+            FollowResponse response = getServerFacade().follow(request, "/unfollow");
+
+            //TODO:: This should remove someone from the following table...
+
+            if (response.isSuccess()) {
+                sendSuccessMessage();
+            } else {
+                sendFailedMessage(response.getMessage());
+            }
+        } catch (TweeterRemoteException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
