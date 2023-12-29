921507ea20c5937fd69e58264e0751541ed45f15
Jared <jared.devenport@gmail.com>
2023-12-29 14:31:24 -0600

    Wrote integration tests for the server facade.

--- app/src/test/java/edu/byu/cs/tweeter/client/model/net/ServerFacadeTest.java
+++ app/src/test/java/edu/byu/cs/tweeter/client/model/net/ServerFacadeTest.java
@@ -1,23 +1,53 @@
 package edu.byu.cs.tweeter.client.model.net;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
+import edu.byu.cs.tweeter.model.net.request.CountRequest;
+import edu.byu.cs.tweeter.model.net.request.FollowersRequest;
+import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
+import edu.byu.cs.tweeter.model.net.response.CountResponse;
+import edu.byu.cs.tweeter.model.net.response.FollowersResponse;
+import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+
 class ServerFacadeTest {
+    public ServerFacade serverFacade;
+    public AuthToken token;
 
     @BeforeEach
     void setUp() {
+        serverFacade = new ServerFacade();
+        token = new AuthToken();
+        token.setToken("token");
     }
 
     @Test
-    void register() {
+    void register() throws IOException, TweeterRemoteException {
+        RegisterRequest request = new RegisterRequest("Jared", "Devenport", "jareddev", "password", "https://students.cs.byu.edu/~jareddev/cs340/icons/boy.png");
+        LoginResponse response =  serverFacade.register(request, "/register");
+        Assertions.assertTrue(response.isSuccess());
+        Assertions.assertNotNull(response.getAuthToken());
+        Assertions.assertNotNull(response.getUser());
     }
 
     @Test
-    void getFollowers() {
+    void getFollowers() throws IOException, TweeterRemoteException {
+        FollowersRequest request = new FollowersRequest(token, "alias", 10, null);
+        FollowersResponse response = serverFacade.getFollowers(request, "/getfollowers");
+        Assertions.assertTrue(response.isSuccess());
+        Assertions.assertNotNull(response.getFollowers());
     }
 
     @Test
-    void count() {
+    void getFollowersCount() throws IOException, TweeterRemoteException {
+        CountRequest request = new CountRequest("targetUserAlias", token, "followers");
+        CountResponse response = serverFacade.count(request, "/count");
+        Assertions.assertTrue(response.isSuccess());
+        Assertions.assertNotNull(response.getCount());
     }
 }
